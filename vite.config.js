856415a330f2b443923efd4398026304f2288309
isacarrd <rcpessoaluso@gmail.com>
2026-08-05 21:02:09 -0300

    update: vite aceitando importação de arquivos .docx

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.docx'],
   base: process.env.VITE_BASE_PATH || "/portfolioIsacarrd"
 })
